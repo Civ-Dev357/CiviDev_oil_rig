caf6b9cd22280fd993a3e5292966c333e969880e
CivDev <gurubuddy188@gmail.com>
2025-10-17 11:29:13 +0530

    Update exports.lua

--- exports.lua
+++ exports.lua
@@ -1,11 +1,1 @@
---============================================================--
--- 🔁 Exports for other scripts to access stock data
---============================================================--
 
-exports('TryBuyFromStock', function(sellId, amount)
-    local cb = promise.new()
-    lib.callback.await('oilrig:server:buyFromStock', false, function(ok)
-        cb:resolve(ok)
-    end, sellId, amount)
-    return cb:wait()
-end)
